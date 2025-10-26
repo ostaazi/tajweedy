39bab9015e67614371f43f2f55cadb53d982b0fd
ostaazi <ostaazi@gmail.com>
2025-10-26 05:28:55 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
-    domains: [
-      'cdn.islamic.network',
-      'verses.quran.com',
-      'api.alquran.cloud'
-    ],
-  },
-  env: {
-    MONGODB_URI: process.env.MONGODB_URI,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    domains: ['cdn.alquran.cloud', 'quran.com', 'api.alquran.cloud'],
   },
   i18n: {
     locales: ['ar'],
     defaultLocale: 'ar',
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
